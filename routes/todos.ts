04b9cdec63b0a1cd6002287d56d551a9d62d2e3e
Isaac Alfaro <isaac16cr@outlook.com>
2023-01-31 18:09:27 -0600

    request types

--- routes/todos.ts
+++ routes/todos.ts
@@ -4,6 +4,9 @@ import { Todo } from '../models/todo';
 
 const router = Router();
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 let todos: Todo[] = [];
 
 router.get('/', (req, res, next) => {
@@ -11,26 +14,29 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/todo', (req, res, next) => {
+  const body = req.body as RequestBody;
   const newTd: Todo = {
     id: new Date().toISOString(),
-    text: req.body.text,
+    text: body.text,
   };
   todos.push(newTd);
   return res.status(200).json({ success: true });
 });
 
 router.put('/todo/:todoId', (req, res, next) => {
-  const tId = req.params.todoId;
-  const todoIdx = todos.findIndex(item => item.id === tId);
+  const params = req.params as RequestParams;
+  const body = req.body as Todo;
+  const todoIdx = todos.findIndex(item => item.id === params.todoId);
   if (todoIdx >= 0) {
-    todos[todoIdx] = { id: todos[todoIdx].id, text: req.body.text };
+    todos[todoIdx] = { id: todos[todoIdx].id, text: body.text };
     return res.status(200).json({ success: true });
   }
   res.status(400).json({ message: 'Could not find id' });
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
-  todos = todos.filter(item => item.id !== req.params.todoId);
+  const params = req.params as RequestParams;
+  todos = todos.filter(item => item.id !== params.todoId);
 
   return res.status(200).json({ success: true });
 });
